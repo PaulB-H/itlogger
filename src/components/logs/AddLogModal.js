108aa55d550facb066b69ffcb6bb811119a52f9b
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2022-02-07 14:10:25 -0500

    Complete structure for AddLogModal, add to app

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const AddLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
+
+  const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "Please enter a message and tech" });
+    } else {
+      console.log(message, tech, attention);
+    }
+  };
 
   return (
     <div id="add-log-modal" className="modal" style={modalStyle}>
@@ -11,7 +20,12 @@ const AddLogModal = () => {
         <h4>Enter System Log</h4>
         <div className="row">
           <div className="input-field">
-            <input type="text" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+            <input
+              type="text"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
             <label htmlFor="message" className="active">
               Log Message
             </label>
@@ -19,8 +33,15 @@ const AddLogModal = () => {
         </div>
         <div className="row">
           <div className="input-field">
-            <select name="tech" value={tech} className="browser-default" onChange={(e) => setTech(e.target.value)}>
-              <option value="" disabled>Select Technician</option>
+            <select
+              name="tech"
+              value={tech}
+              className="browser-default"
+              onChange={(e) => setTech(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Technician
+              </option>
               <option value="John Doe">John Doe</option>
               <option value="Sam Smith">Sam Smith</option>
               <option value="Sara Wilson">Sara Wilson</option>
@@ -31,21 +52,35 @@ const AddLogModal = () => {
           <div className="input">
             <p>
               <label>
-                <input type="checkbox" className="filled-in" checked={attention} value={attention} />
+                <input
+                  type="checkbox"
+                  className="filled-in"
+                  checked={attention}
+                  value={attention}
+                  onChange={(e) => setAttention(!attention)}
+                />
+                <span>Needs Attention</span>
               </label>
             </p>
           </div>
         </div>
       </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect blue waves-light btn"
+        >
+          Enter
+        </a>
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 const modalStyle = {
   width: "75%",
   height: "75%",
-}
+};
 
-export default AddLogModal
-
+export default AddLogModal;
